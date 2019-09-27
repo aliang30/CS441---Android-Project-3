4b1f71ad802c9e879d9468d24dbbd127b80c7109
aliang30 <aliang30@binghamton>
2019-09-26 23:56:15 -0400

    Git commit

--- app/src/main/java/com/example/myapplication3/Canvas.java
+++ app/src/main/java/com/example/myapplication3/Canvas.java
@@ -13,14 +13,16 @@ public class Canvas extends View {
 
     public Canvas(Context context) {
         super(context);
+        paint = new Paint();
+        rect = new Rect();
     }
 
     @Override
     protected void onDraw(android.graphics.Canvas canvas) {
         super.onDraw(canvas);
         paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(2);
+        paint.setStrokeWidth(3);
 
-        canvas.drawRect(0, 0, canvas.getWidth(),canvas.getHeight()/3,paint);
+        canvas.drawRect(2, 2, canvas.getWidth()/2,canvas.getHeight()/2,paint);
     }
 }
